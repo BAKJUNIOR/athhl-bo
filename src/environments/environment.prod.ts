20be38d46d455cd24db78e1a17be1b3cce88c98b
bakbassjunior <bakbassjunior@gmail.com>
2026-09-24 15:32:46 +0000

    Corrige l'URL de l'API en prod : api.athl.athl-logistique.com (l'ancien domaine africatalentprojets.tech ne résout plus)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'https://api.athl-logistics.africatalentprojets.tech',
+  apiUrl: 'http://api.athl.athl-logistique.com',
   cloudinary: {
     cloudName: 'drfq0bt4z',
     uploadPreset: 'xbanking'
